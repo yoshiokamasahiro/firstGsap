ddbf1abda505ab800bfc8b130dc1357554f80980
yoshiokamasahiro <34766492+yoshiokamasahiro@users.noreply.github.com>
2022-09-12 01:20:33 +0900

    ScrollTrigger first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import gsap from "gsap";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import "./styles.css";
 
-gsap.to(".box01", {
+const box01 = document.querySelectorAll(".box01");
+gsap.to(box01, {
   duration: 2,
   x: 500,
   rotate: 360,
   repeat: -1
 });
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.fromTo(
+  ".js-animation-fadein",
+  {
+    y: -50,
+    autoAlpha: 0
+  },
+  {
+    y: 0,
+    autoAlpha: 1,
+    delay: 1,
+    duration: 2,
+    //repeat: -1, //-1はリピート
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".js-animation-fadein",
+      start: "top center"
+    }
+  }
+);
